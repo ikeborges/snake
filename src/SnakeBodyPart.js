c7a8f5994cf3ca48a3031891fab0f689a278db38
Kaique "Kai" Borges <rique.kaique@gmail.com>
2022-11-16 16:16:12 -0300

    Adds tests to SnakeBodyPart class

---
+++ src/SnakeBodyPart.js
@@ -0,0 +1,56 @@
+import { runInThisContext } from "vm"
+import Directions from "./Directions.js"
+
+class SnakeBodyPart {
+  constructor({ direction, position }) {
+    this.direction = direction
+    this.position = position
+  }
+
+  getCoordinates() {
+    return this.position
+  }
+
+  getDirection() {
+    return this.direction
+  }
+
+  turnTo(direction) {
+    this.direction = direction
+  }
+
+  moveOneStep({ MAX_WIDTH_IN_BLOCKS, MAX_HEIGHT_IN_BLOCKS }) {
+    const lastYPosition = MAX_HEIGHT_IN_BLOCKS - 1
+    const lastXPosition = MAX_WIDTH_IN_BLOCKS - 1
+
+    // The Y axis in canvas is upside down
+    switch (this.direction) {
+      case Directions.UP:
+        if (this.position.yCoordinate === 0)
+          this.position.yCoordinate = lastYPosition
+        else this.position.yCoordinate -= 1
+
+        break
+      case Directions.DOWN:
+        if (this.position.yCoordinate === lastYPosition)
+          this.position.yCoordinate = 0
+        else this.position.yCoordinate += 1
+
+        break
+      case Directions.LEFT:
+        if (this.position.xCoordinate === 0)
+          this.position.xCoordinate = lastXPosition
+        else this.position.xCoordinate -= 1
+
+        break
+      case Directions.RIGHT:
+        if (this.position.xCoordinate === lastXPosition)
+          this.position.xCoordinate = 0
+        else this.position.xCoordinate += 1
+
+        break
+    }
+  }
+}
+
+export default SnakeBodyPart
