8117c77a9bb195812ff172fb217c1ff22acc6dba
Kaique Borges <rique.kaique@gmail.com>
2023-02-17 12:03:50 -0300

    test: oppositeDirection helper function

---
+++ v2/src/Direction.spec.ts
@@ -0,0 +1,17 @@
+import { assert, describe, it } from "vitest";
+import Direction, { oppositeDirection } from "./Direction";
+
+describe("oppositeDirection(direction)", () => {
+  it("should return the opposite direction of its parameter", () => {
+    const oppositeDirections = new Map([
+      [Direction.UP, Direction.DOWN],
+      [Direction.DOWN, Direction.UP],
+      [Direction.LEFT, Direction.RIGHT],
+      [Direction.RIGHT, Direction.LEFT],
+    ]);
+
+    for (let [direction, opposite] of oppositeDirections) {
+      assert.strictEqual(oppositeDirection(direction), opposite);
+    }
+  });
+});
