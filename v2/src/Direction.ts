1639e599e214d1029e3e60c3a004fb38b35d330d
Kaique Borges <rique.kaique@gmail.com>
2023-02-17 12:05:00 -0300

    feat: oppositeDirection helper function

--- v2/src/Direction.ts
+++ v2/src/Direction.ts
@@ -1,8 +1,21 @@
 enum Direction {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
+  UP = "UP",
+  DOWN = "DOWN",
+  LEFT = "LEFT",
+  RIGHT = "RIGHT",
 }
+
+export const oppositeDirection = (of: Direction) => {
+  switch (of) {
+    case Direction.UP:
+      return Direction.DOWN;
+    case Direction.DOWN:
+      return Direction.UP;
+    case Direction.LEFT:
+      return Direction.RIGHT;
+    case Direction.RIGHT:
+      return Direction.LEFT;
+  }
+};
 
 export default Direction;
