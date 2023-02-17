20ce8dc83fc1a512ad250c011e49eb709c51674b
Kaique Borges <rique.kaique@gmail.com>
2023-02-17 14:23:03 -0300

    feat: test: snake directions change restrictions

--- v2/src/Snake.ts
+++ v2/src/Snake.ts
@@ -1,12 +1,35 @@
-import Direction from "./Direction";
+import Direction, { oppositeDirection } from "./Direction";
+
+export interface InitialSnakeState {
+  size: number;
+  direction: Direction;
+}
 
 class Snake {
-  public size: number;
-  public direction: Direction;
+  private _size: number;
+  private _direction: Direction;
 
-  constructor() {
-    this.size = 3;
-    this.direction = Direction.UP;
+  constructor({ size, direction }: InitialSnakeState) {
+    this._size = size;
+    this._direction = direction;
+  }
+
+  turnTo(direction: Direction): Direction {
+    if (direction === oppositeDirection(this._direction)) {
+      return this._direction;
+    }
+
+    this._direction = direction;
+
+    return this._direction;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get direction() {
+    return this._direction;
   }
 }
 
