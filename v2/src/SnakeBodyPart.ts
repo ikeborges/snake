40f534222f4d51232193c71c9726abfae8be3d37
Kaique Borges <rique.kaique@gmail.com>
2023-02-20 22:10:45 -0300

    Updates SnakeBodyPart

--- v2/src/SnakeBodyPart.ts
+++ v2/src/SnakeBodyPart.ts
@@ -1,8 +1,50 @@
 import Direction from "./Direction";
+import GameSettings from "./GameSettings";
 import Position from "./Position";
 
 class SnakeBodyPart {
   constructor(public position: Position, public direction: Direction) {}
+
+  getPosition() {
+    return this.position;
+  }
+
+  getDirection() {
+    return this.direction;
+  }
+
+  turnTo(direction) {
+    this.direction = direction;
+  }
+
+  moveOneStep() {
+    const lastYPosition = GameSettings.HEIGHT_IN_BLOCKS - 1;
+    const lastXPosition = GameSettings.WIDTH_IN_BLOCKS - 1;
+
+    // The Y axis in canvas is upside down, so to go up we need to subtract from Y
+    switch (this.direction) {
+      case Direction.UP:
+        if (this.position.y === 0) this.position.y = lastYPosition;
+        else this.position.y -= 1;
+
+        break;
+      case Direction.DOWN:
+        if (this.position.y === lastYPosition) this.position.y = 0;
+        else this.position.y += 1;
+
+        break;
+      case Direction.LEFT:
+        if (this.position.x === 0) this.position.x = lastXPosition;
+        else this.position.x -= 1;
+
+        break;
+      case Direction.RIGHT:
+        if (this.position.x === lastXPosition) this.position.x = 0;
+        else this.position.x += 1;
+
+        break;
+    }
+  }
 }
 
 export default SnakeBodyPart;
