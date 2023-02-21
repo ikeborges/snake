7173dbe0b0d41c7cca4d9118767a29d94cbef3a8
Kaique Borges <rique.kaique@gmail.com>
2023-02-20 22:08:40 -0300

    Updates Position class

--- v2/src/Position.ts
+++ v2/src/Position.ts
@@ -1,5 +1,22 @@
+import GameSettings from "./GameSettings";
+
 class Position {
   constructor(public x: number, public y: number) {}
+
+  static generateRandomPosition() {
+    const xCoordinate = Math.floor(
+      Math.random() * GameSettings.WIDTH_IN_BLOCKS
+    );
+    const yCoordinate = Math.floor(
+      Math.random() * GameSettings.HEIGHT_IN_BLOCKS
+    );
+
+    return new Position(xCoordinate, yCoordinate);
+  }
+
+  equals(position: Position): boolean {
+    return this.x === position.x && this.y === position.y;
+  }
 }
 
 export default Position;
