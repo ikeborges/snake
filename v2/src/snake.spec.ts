0048e228b97eebe038faae9e44edf1d734cc411d
Kaique Borges <rique.kaique@gmail.com>
2023-02-20 21:58:44 -0300

    test: eating behavior

--- v2/src/snake.spec.ts
+++ v2/src/snake.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it, assert, beforeEach } from "vitest";
 import Direction, { oppositeDirection } from "./Direction";
+import Position from "./Position";
 import Snake, { InitialSnakeState } from "./Snake";
 
 describe("Snake", () => {
@@ -45,6 +46,77 @@ describe("Snake", () => {
         const updatedDirection = snake.turnTo(opposite);
         assert.strictEqual(updatedDirection, direction);
       });
+    });
+  });
+
+  describe("body", () => {
+    it("should increase size when it eats", () => {
+      const initialSize = 3;
+      const expected = 4;
+
+      snake = new Snake({ size: initialSize, direction: Direction.UP });
+      snake.eat();
+      const actual = snake.size;
+
+      assert.strictEqual(actual, expected);
+    });
+
+    it("should have new body part appended to the end when it eats", () => {
+      snake = new Snake({ size: 2, direction: Direction.UP });
+
+      snake.eat();
+      snake.eat();
+
+      assert.strictEqual(snake.body.length, 4);
+    });
+
+    it("should have new body part with the same direction as the previous tail", () => {
+      snake = new Snake({ size: 3, direction: Direction.DOWN });
+
+      const tailBeforeEating = snake.tail;
+      snake.eat();
+      const tailAfterEating = snake.tail;
+
+      assert.strictEqual(tailAfterEating.direction, tailBeforeEating.direction);
+    });
+
+    it("should have new body part in a position next to the previous tail", () => {
+      snake = new Snake({ size: 3, direction: Direction.UP });
+
+      const tailBeforeEating = snake.tail;
+      snake.eat();
+      const tailAfterEating = snake.tail;
+
+      let expected: Position;
+
+      switch (tailBeforeEating.direction) {
+        case Direction.UP:
+          expected = {
+            x: tailBeforeEating.position.x,
+            y: tailBeforeEating.position.y - 1,
+          };
+          break;
+        case Direction.DOWN:
+          expected = {
+            x: tailBeforeEating.position.x,
+            y: tailBeforeEating.position.y + 1,
+          };
+          break;
+        case Direction.LEFT:
+          expected = {
+            x: tailBeforeEating.position.x + 1,
+            y: tailBeforeEating.position.y,
+          };
+          break;
+        case Direction.RIGHT:
+          expected = {
+            x: tailBeforeEating.position.x - 1,
+            y: tailBeforeEating.position.y,
+          };
+          break;
+      }
+
+      assert.deepEqual(tailAfterEating.position, expected);
     });
   });
 });
