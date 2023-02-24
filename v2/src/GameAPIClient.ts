ceb2790baa26d05b2bc96ac6924aa7ffd9e00b13
Kaique Borges <rique.kaique@gmail.com>
2023-02-24 16:11:50 -0300

    Removes backend api

---
+++ v2/src/GameAPIClient.ts
@@ -0,0 +1,21 @@
+export namespace GameAPIClient {
+  const API_URL = "";
+
+  export async function saveStatsInRanking(
+    playerName: string,
+    score: number,
+    elapsedTime: number
+  ) {
+    // return fetch(
+    //   `${API_URL}/ranking?playerName=${playerName}&score=${score}&elapsedTime=${elapsedTime}`,
+    //   {
+    //     method: "POST",
+    //   }
+    // );
+  }
+
+  export async function getRanking() {
+    // return fetch(`${API_URL}/ranking`, {
+    // });
+  }
+}
