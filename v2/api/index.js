c7f9fffa821014a8119ce54a85643bf558a220f6
Kaique Borges <rique.kaique@gmail.com>
2023-02-24 15:19:09 -0300

    Removes cors package

--- v2/api/index.js
+++ v2/api/index.js
@@ -1,16 +1,9 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import dbClient from "./dbClient.js";
 
 dotenv.config();
 const app = express();
-
-app.use(
-  cors({
-    origin: process.env.GAME_URL,
-  })
-);
 
 app.get("/", (req, res) => {
   res.send({ hello: "world" });
