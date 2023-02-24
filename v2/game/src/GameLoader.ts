892b453adb2b031df48d1319163889dbebef9ef3
Kaique Borges <rique.kaique@gmail.com>
2023-02-24 14:28:50 -0300

    Compiles frontend for GH Pages

--- v2/game/src/GameLoader.ts
+++
@@ -1,9 +0,0 @@
-import CanvasGame from "./CanvasGame";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
-  const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-
-  const game = new CanvasGame(window, document, context);
-  game.start();
-});
--- v2/game/src/GameSettings.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  FRAME_HEIGHT: 600,
-  FRAME_WIDTH: 400,
-  BLOCK_SIZE: 20,
-};
