bfcd314c6df01105c49ea383c8c783e90ab0356f
Kaique Borges <rique.kaique@gmail.com>
2023-02-24 23:24:43 -0300

    Removes version subfolder

--- v1/src/DirectionChange.js
+++
@@ -1,8 +0,0 @@
-class DirectionChange {
-  constructor({ direction, position }) {
-    this.direction = direction
-    this.position = position
-  }
-}
-
-export default DirectionChange
--- v1/src/Food.js
+++
@@ -1,9 +0,0 @@
-import Position from "./Position.js"
-
-class Food {
-  constructor(position = Position.generateRandomPosition()) {
-    this.position = new Position({ ...position })
-  }
-}
-
-export default Food
